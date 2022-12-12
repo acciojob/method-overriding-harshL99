4c8ab4ff39bbab5df86b23edb8b7af1c54e000dd
Harsh Lalwani <harshlalwani660@gmail.com>
2022-12-12 16:08:39 +0530

    Committed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
+class A{
+    public String Meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A{
+
+    public void callAMeth(){
+        super.Meth();
+    }
+    @Override
+    public String Meth(){
+        return "Method is overridden in Extendend class B";
+    }
+}
+
 public class Main {
-  
+   public static void main(String[] args){
+       B b=new B();
+       b.callAMeth();
+       b.Meth();
+   }
 }
